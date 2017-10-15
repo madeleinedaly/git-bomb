a72bf9ff3a8d889ed030c881d8fb88c636bd4d85
Madeleine Daly <mdaly@lifeimage.com>
2017-10-15 11:41:47 -0400

    remove extra const

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const tmpFile = promisify(require('tmp').file);
 const writeFile = promisify(require('fs').writeFile);
 const exec = promisify(require('child_process').execFile);
 
-// write a git object and return the hash
+// writes a git object and returns the hash
 const writeObject = async (objectBody, { type }) => {
   const path = await tmpFile();
   await writeFile(path, objectBody);
@@ -16,7 +16,7 @@ const writeObject = async (objectBody, { type }) => {
   return stdout.replace(/\n/, '');
 };
 
-// write a git commit and return the hash
+// writes a git commit and returns the hash
 const writeCommit = async treeHash => {
   const commitMessage = '"create a git bomb"';
   const { stdout } = await exec('git', ['commit-tree', '-m', commitMessage, treeHash]);
@@ -25,6 +25,7 @@ const writeCommit = async treeHash => {
 
 const by = key => (a, b) => a[key] === b[key] ? 0 : a[key] < b[key] ? 1 : -1;
 
+// makes a valid git tree object
 const createTree = (dirs, perm) => {
   const tree = dirs.sort(by('name')).reduce((prevBuffer, { name, hash }) => {
     const nextBuffer = [
@@ -64,7 +65,7 @@ if (require.main === module) {
 
     // make layers of tree objects using the previous tree object
     // each tree contains the last tree `width` times
-    const treeHash = await asyncReduce(range(depth - 1), async (prevHash, i) => {
+    const treeHash = await asyncReduce(range(depth - 1), async prevHash => {
       const otherDirs = range(width).map(i => ({name: `d${i}`, hash: prevHash}));
 
       const nextHash = await writeObject(createTree(otherDirs, treePerms), {type: 'tree'});
